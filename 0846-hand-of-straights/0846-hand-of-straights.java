c3772713b09a2971ee3461c3a450a86be94c60e4
kalkikumar57 <132195475+kalkikumar57@users.noreply.github.com>
2024-06-06 09:39:14 +0530

    Time: 8 ms (100.00%), Space: 44.6 MB (98.74%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean findSuccessor(int[] hand, int groupSize,int i,int n) {
+        int f = hand[i]+1;
+        hand[i]=-1;
+        int count=1;
+        i=i+1;
+        while(i<n && count < groupSize ){
+            if(hand[i] ==f){
+                f=hand[i]+1;
+                hand[i] = -1;
+                count++;
+            }
+            i++;
+        }
+        if(count != groupSize)
+            return false;
+        else
+            return true;
+    }
+    public boolean isNStraightHand(int[] hand,int groupSize){
+        int n = hand.length;
+        if( n % groupSize != 0)
+            return false;
+        Arrays.sort(hand);
+        for(int i=0;i<n;i++){
+            if(hand[i] >= 0 ){
+                if(!findSuccessor(hand,groupSize,i,n))
+                    return false; 
+            }
+        }
+        return true;
+    }
+}
