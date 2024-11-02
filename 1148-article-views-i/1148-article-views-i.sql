b18dbb532b59269092661f93ff9c1a745bf06e2e
kalkikumar57 <132195475+kalkikumar57@users.noreply.github.com>
2024-11-02 13:17:49 +0530

    Time: 433 ms (75.29%), Space: 0B (100.00%) - LeetHub

---
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -0,0 +1,9 @@
+SELECT DISTINCT author_id AS id
+FROM Views
+WHERE   author_id=viewer_id 
+ORDER BY id
+
+
+# select distinct author_id as id from Views
+# where author_id = viewer_id 
+# order by id;
