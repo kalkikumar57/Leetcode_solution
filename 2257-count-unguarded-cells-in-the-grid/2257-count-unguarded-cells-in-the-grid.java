232145b86e1e065f12946ce144e760e046593458
kalkikumar57 <132195475+kalkikumar57@users.noreply.github.com>
2024-11-21 18:01:37 +0530

    Time: 21 ms (78.95%), Space: 71.9 MB (34.94%) - LeetHub

---
+++ 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.java
@@ -0,0 +1,72 @@
+class Solution {
+    public void markGuarded(int row, int col, int[][] grid) {
+       
+        for (int i = row - 1; i >= 0; i--) {
+            if (grid[i][col] == 2 || grid[i][col] == 3) {
+                break;
+            }
+            grid[i][col] = 1; 
+        }
+
+ 
+        for (int i = row + 1; i < grid.length; i++) {
+            if (grid[i][col] == 2 || grid[i][col] == 3) {
+                break;
+            }
+            grid[i][col] = 1; 
+        }
+
+     
+        for (int j = col - 1; j >= 0; j--) {
+            if (grid[row][j] == 2 || grid[row][j] == 3) {
+                break;
+            }
+            grid[row][j] = 1; 
+        }
+
+       
+        for (int j = col + 1; j < grid[0].length; j++) {
+            if (grid[row][j] == 2 || grid[row][j] == 3) {
+                break;
+            }
+            grid[row][j] = 1; 
+        }
+    }
+
+    public int countUnguarded(int m, int n, int[][] guards, int[][] walls) {
+        int[][] grid = new int[m][n];
+
+
+        for (int[] guard : guards) {
+            int i = guard[0];
+            int j = guard[1];
+            grid[i][j] = 2; 
+        }
+
+    
+        for (int[] wall : walls) {
+            int i = wall[0];
+            int j = wall[1];
+            grid[i][j] = 3; 
+        }
+
+      
+        for (int[] guard : guards) {
+            int i = guard[0];
+            int j = guard[1];
+            markGuarded(i, j, grid);
+        }
+
+        // Count unguarded cells
+        int count = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 0) { 
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
