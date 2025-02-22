e04e68136e33626127ea3b1ccdeec3a6a75fb88e
kalkikumar57 <132195475+kalkikumar57@users.noreply.github.com>
2025-02-22 20:48:17 +0530

    Time: 0 ms (100%), Space: 42.5 MB (78.17%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -1,37 +1,40 @@
-// *
-//  * Definition for singly-linked list.
-//  * public class ListNode {
-//  *     int val;
-//  *     ListNode next;
-//  *     ListNode() {}
-//  *     ListNode(int val) { this.val = val; }
-//  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-//  * }
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public static ListNode findNthNode(ListNode temp,int k){
-        int count=1;
-        while(temp != null){
-            if(count == k)return temp;
-            count++;
-            temp = temp.next;
-        }
-        return temp;
+    int count=1;
+    while(temp!=null){
+        if(count == k) return temp;
+        count++;
+        temp=temp.next;
     }
+    return temp;
+}
     public ListNode rotateRight(ListNode head, int k) {
         if(head == null || k==0) return head;
-        ListNode tail = head;
-        int len = 1;
+        ListNode tail=head;
+        int length=1;
         while(tail.next != null){
-            tail = tail.next;
-            len = len+1;
+            tail=tail.next;
+            length +=1;
         }
-        if(k % len == 0) return head;
-        k=k%len;
-        tail.next = head;
-        ListNode newLastNode = findNthNode(head,len-k);
+        if(k % length == 0) return head;
+        k=k%length;
+        tail.next =head;
+        ListNode newLastNode = findNthNode(head,length-k);
+
         head = newLastNode.next;
         newLastNode.next = null;
+
         return head;
+
     }
 }
