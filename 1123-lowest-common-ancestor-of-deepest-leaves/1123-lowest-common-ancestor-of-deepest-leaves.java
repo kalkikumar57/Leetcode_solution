f5924a48a275fa4660026a25f8705cdae1fbb4ad
kalkikumar57 <132195475+kalkikumar57@users.noreply.github.com>
2025-04-04 15:01:48 +0530

    Time: 0 ms (100%), Space: 44.6 MB (28.54%) - LeetHub

---
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    class Pair {
+        int depth;
+        TreeNode node;
+
+        Pair(int depth, TreeNode node) {
+            this.depth = depth;
+            this.node = node;
+        }
+    }
+
+    Pair solve(TreeNode root) {
+        if (root == null) {
+            return new Pair(0, null);
+        }
+
+        Pair l = solve(root.left);
+        Pair r = solve(root.right);
+
+        if (l.depth == r.depth) {
+            return new Pair(l.depth + 1, root);
+        } else if (l.depth > r.depth) {
+            return new Pair(l.depth + 1, l.node);
+        } else {
+            return new Pair(r.depth + 1, r.node);
+        }
+    }
+
+    public TreeNode lcaDeepestLeaves(TreeNode root) {
+        return solve(root).node;
+    }
+}
