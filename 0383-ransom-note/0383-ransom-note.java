b21e380b20fcd91f48fcfdc40be5411e29190c2f
kalkikumar57 <132195475+kalkikumar57@users.noreply.github.com>
2026-05-02 00:18:05 +0530

    Time: 16 ms (18.61%), Space: 47 MB (35.36%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> frq = new HashMap<>();
+
+        for (int i = 0; i < magazine.length(); i++) {
+            char ch = magazine.charAt(i);
+            frq.put(ch, frq.getOrDefault(ch, 0) + 1);
+        }
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+            char ch = ransomNote.charAt(i);
+
+            if (frq.getOrDefault(ch, 0) == 0) {
+                return false;
+            }
+
+            frq.put(ch, frq.get(ch) - 1);
+        }
+        return true;
+    }
+}
